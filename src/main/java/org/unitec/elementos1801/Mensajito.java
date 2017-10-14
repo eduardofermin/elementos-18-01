0c6be5b7a48f2294174f21d83a44ff9a620eeaae
T-107 <T-107@PC183>
2017-10-13 21:42:56 -0500

    13/10/2017

---
+++ src/main/java/org/unitec/elementos1801/Mensajito.java
@@ -0,0 +1,73 @@
+/*
+
+   ****MAPEO DE LA ENTIDAD "mensajito"****
+
+ */
+package org.unitec.elementos1801;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author T-107
+ */
+
+@Entity
+public class Mensajito {
+    
+    //notaciones del id
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private String titulo;
+    private String cuerpo;
+    
+    //constructores
+
+    public Mensajito() {
+    }
+
+    public Mensajito(Integer id, String titulo, String cuerpo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensajito(Integer id) {
+        this.id = id;
+    }
+
+    public Mensajito(String titulo, String cuerpo) {
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+    
+    //encapsulacion de los campos
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+    
+}
---
+++ src/main/java/org/unitec/elementos1801/tablitas.sql
@@ -0,0 +1,4 @@
+--creacion de la entidad
+create table mensajito(id integer primary key auto_increment,titulo varchar(30),cuerpo varchar(250));
+
+select *from mensajito;
